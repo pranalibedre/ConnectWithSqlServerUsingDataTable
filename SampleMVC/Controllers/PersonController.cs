0dd0067421d848f97789804dbe63e46c7057c417
Niteen Sapkal <niteensapkal@necsws.com>
2022-07-06 19:27:23 +0530

    added listing

--- SampleMVC/Controllers/PersonController.cs
+++ SampleMVC/Controllers/PersonController.cs
@@ -17,37 +17,40 @@ namespace SampleMVC.Controllers
         { 
             List<Person> getPersonDetails = new List<Person>();
             IPersonRepository personRepository = new PersonRepository();
-            Person person = new Person();
             getPersonDetails = personRepository.GetPerson();
-            PersonContext personContext = new PersonContext();
-            person.FirstName = personContext.FirstName;
-            person.LastName = personContext.LastName;
-            person.Gender = new Gender
-            {
-                GenderName = personContext.Gender
-            };
-            person.Age = personContext.Age;
-            person.Email = personContext.Email;
-            person.DateOfBirth = Convert.ToDateTime(personContext.DateOfBirth);
-            person.TelephoneNo = personContext.TelephoneNo;
-            person.PersonAddress = new PersonAddress
-            {
-                AddressLine1 = personContext.AddressLine1,
-                AddressLine2 = personContext.AddressLine2,
-                City = personContext.City,
-                PinCode = personContext.PinCode
-            };
-            person.Country = new Country
-            {
-                CountryName = personContext.SelectedCountry
-            };
-            person.State = new State
-            {
-                StateName = personContext.SelectedState
-            };
-            getPersonDetails.Add(person);
 
-            return View(getPersonDetails);
+            List<PersonContext> peopleContext = new List<PersonContext>();
+            foreach (var person in getPersonDetails)
+            {
+                var personContext = new PersonContext();
+                personContext.FirstName = person.FirstName;
+                personContext.LastName = person.LastName;
+                //personContext.Gender = new Gender
+                //{
+                //    GenderName = personContext.Gender
+                //};
+                personContext.Age = person.Age;
+                personContext.Email = person.Email;
+                personContext.DateOfBirth = Convert.ToDateTime(person.DateOfBirth);
+                personContext.TelephoneNo = person.TelephoneNo;
+                //personContext.PersonAddress = new PersonAddress
+                //{
+                //    AddressLine1 = personContext.AddressLine1,
+                //    AddressLine2 = personContext.AddressLine2,
+                //    City = personContext.City,
+                //    PinCode = personContext.PinCode
+                //};
+                //personContext.Country = new Country
+                //{
+                //    CountryName = personContext.SelectedCountry
+                //};
+                //personContext.State = new State
+                //{
+                //    StateName = personContext.SelectedState
+                //};
+                peopleContext.Add(personContext);
+            }
+            return View(peopleContext);
         }
 
         public ActionResult Index()
